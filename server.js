af91fe8722a9dffedd9c10170f583cd98f23f572
Priyam <priyam009@live.com>
2019-09-22 21:05:38 +1000

    Add and start server

---
+++ server.js
@@ -0,0 +1,23 @@
+//Express
+var express = require('express');
+
+//Set Port
+var PORT = process.env.PORT || 3000;
+
+//Initialise express
+var app = express();
+
+//Parse
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+//Express handlebars
+var exphbs = require('express-handlebars');
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars")
+
+//Start server
+app.listen(PORT, function() {
+  console.log("Server listening on https://localhost" + PORT);
+})
