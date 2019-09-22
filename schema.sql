fe88ef46fc8075dd86d0b9b8f4d275d700b121b3
Priyam <priyam009@live.com>
2019-09-22 20:27:39 +1000

    Add database schema and create only the database- 'burgersq_db'

---
+++ schema.sql
@@ -0,0 +1,9 @@
+-- Drop burgers database if exists
+DROP DATABASE IF EXISTS burgersq_db;
+
+-- Create burger database for sequelize
+CREATE DATABASE burgersq_db;
+
+--Use burgersq database
+USE burgersq_db;
+
